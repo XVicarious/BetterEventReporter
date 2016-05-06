9aa4b655ffcbaf9ec22fa8617ab3ca561449f92f
Brian Maurer <bmaurer@xvss.net>
2016-05-06 13:49:53 -0400

    fixed an oopsie...

--- js/Player.js
+++ js/Player.js
@@ -1,41 +1,39 @@
 var Player = function(id) {
   this.id = id;
-  // rounds = [[player, wins]]
-  this.rounds = [];
-};
-
-Player.wins = function() {
-  var total = 0;
-  this.rounds.forEach(function(round){
-    total += round[1];
-  });
-  return total;
-};
-
-Player.losses = function() {
-  var total = 0;
-  for (var i = 0; i < this.rounds.length; i++) {
-    var roundWins = this.rounds[i][1];
-    if (Math.round(roundWins) === roundWins && roundWins === 1) {
-      // if there is no tie, and the player won 1 game
-      total += 2;
-    } else if (roundWins === 2 && this.rounds[0].rounds[i][1] === 1) {
-      // if the player won 2 games, and his/her opponent won 1 game
-      total += 1;
+  this.rounds = [];  // array of array of [player, wins]
+  this.wins = function() {
+    var total = 0;
+    this.rounds.forEach(function(round){
+      total += round[1];
+    });
+    return total;
+  };
+  this.losses = function() {
+    var total = 0;
+    for (var i = 0; i < this.rounds.length; i++) {
+      var roundWins = this.rounds[i][1];
+      if (Math.round(roundWins) === roundWins && roundWins === 1) {
+        // if there is no tie, and the player won 1 game
+        total += 2;
+      } else if (roundWins === 2 && this.rounds[i][0].rounds[i][1] === 1) {
+        // if the player won 2 games, and his/her opponent won 1 game
+        total += 1;
+      }
+      // otherwise, good job guy!
     }
-    // otherwise, good job guy!
+    return total;
+  };
+  this.ratio = function() {
+    return (this.wins() / this.losses());
+  };
+  this.opponentRatio = function() {
+    var totalRatio = 0;
+    this.rounds.forEach(function(round){
+      totalRatio = round[0].ratio();
+    });
+    return (totalRatio / this.rounds.length);
+  };
+  this.addRound = function(opponent, wins) {
+    this.rounds.push([opponent, wins]);
   }
-  return total;
-};
-
-Player.ratio = function() {
-  return (this.wins() / this.losses());
-};
-
-Player.opponentRatio = function() {
-  var totalRatio = 0;
-  this.rounds.forEach(function(round){
-    totalRatio = round[0].ratio();
-  });
-  return (totalRatio / this.rounds.length);
 };
